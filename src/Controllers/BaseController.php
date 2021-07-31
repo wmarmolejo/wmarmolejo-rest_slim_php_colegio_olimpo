6f02fafc034baba88d8a1fa448a759a4a8d7073a
wmarmolejo <wil.marodri@hotmail.com>
2021-07-31 10:55:00 -0500

    basecontroller

---
+++ src/Controllers/BaseController.php
@@ -0,0 +1,13 @@
+<?php namespace App\Controllers;
+
+use Psr\Container\ContainerInterface;
+
+class BaseController{
+
+  protected $container;
+
+  public function __construct(ContainerInterface $c){
+    $this->container= $c;
+  }
+
+}
